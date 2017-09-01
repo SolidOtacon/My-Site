97e05c30b675c0adc09053ed92dd0e5e9532bccb
Trent Matthias <trent_matthias@hotmail.com>
2017-09-01 14:13:05 -0800

    Fixed themes

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,7 +4,7 @@ import { ILink } from './../models/link.model';
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
