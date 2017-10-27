5100e9ecf4b74e25081e33eb21fb7b18c1a8cdd9
Trent Matthias <tmatthias@gci.com>
2017-10-26 16:08:50 -0800

    Removed unneeded imports

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -1,19 +1,14 @@
 import { AppComponent } from './app.component';
 import { routes } from './../../../testing-utils/stubs/router/router-stubs';
-import { StoreModule, Store } from '@ngrx/store';
-import { State } from './../ngrx/layout/layout.reducers';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { SimpleComponent } from '../../../testing-utils/mock-components/simple/simple.component';
 import { TestModule } from '../../../testing-utils/modules/test.module';
-import { TestBed, ComponentFixture, async, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { Router, Routes, NavigationEnd } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Location } from '@angular/common';
-import * as fromApp from '../ngrx/app.reducers';
-import * as LayoutAction from '../ngrx/layout/layout.actions';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<TestWrapperComponent>;
