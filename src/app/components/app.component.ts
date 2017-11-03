9953fad0ce0908ee7c45bdc098c8c3a7fbc39a0d
Trent Matthias <tmatthias@gci.com>
2017-11-03 13:04:18 -0800

    reverted last change.

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,8 +1,7 @@
-import { Subscription } from 'rxjs/Subscription';
 import { MatSidenav } from '@angular/material';
 import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 import { IMedia } from './../models/media.model';
-import { Component, Input, ChangeDetectionStrategy, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { ILink } from './../models/link.model';
 
 @Component({
@@ -12,31 +11,11 @@ import { ILink } from './../models/link.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   @ViewChild('nav') nav: MatSidenav;
   @Input() title: string;
   @Input() buttonList: Array<ILink>;
   @Input() isMobileView: boolean;
-  watcher: Subscription;
-
-  constructor(
-    private media: ObservableMedia
-  ) {}
-
-  ngOnInit() {
-    this.watcher = this.media.subscribe((change: MediaChange) => {
-      if (!(change.mqAlias === 'xs' || change.mqAlias === 'sm' || change.mqAlias === 'md') && !this.nav.opened) {
-        this.nav._animationState = 'open-instant';
-        this.nav.open();
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.watcher) {
-      this.watcher.unsubscribe();
-    }
-  }
 
   onLinkClick(): void {
     if (this.isMobileView) {
